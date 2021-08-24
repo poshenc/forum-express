5ce9b485265e426a0f9828a946f69ccbfb869a06
johnson <poshenc@gmail.com>
2021-08-24 15:20:53 +0800

    add signup function

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require('express')
 const handlebars = require('express-handlebars') // 引入 handlebars
+const db = require('./models') // 引入資料庫
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = 3000
 
-app.engine('handlebars', handlebars()) // Handlebars 註冊樣板引擎
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
+  // db.sequelize.sync()
   console.log(`Example app listening on port ${port}!`)
 })
 
-// require('./routes')(app)
+require('./routes')(app)
 
-// module.exports = app
+module.exports = app
