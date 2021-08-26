7397420c5ab24f74ee66e1881a29ce91e4b18370
johnson <poshenc@gmail.com>
2021-08-26 17:41:36 +0800

    add admin restaurant put feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -39,6 +39,34 @@ const adminController = {
         req.flash('success_messages', 'restaurant was successfully created')
         res.redirect('/admin/restaurants')
       })
+  },
+
+  editRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id, { raw: true })
+    .then(restaurant => {
+      return res.render('admin/create', { restaurant : restaurant})
+    })
+  },
+
+  putRestaurant: (req, res) => {
+    if(!req.body.name){
+      req.flash('error_messages', "please enter name of restaurant")
+      return res.redirect('back')
+    }
+    return Restaurant.findByPk(req.params.id)
+    .then((restaurant) => {
+      restaurant.update({
+        name: req.body.name,
+        tel: req.body.tel,
+        address: req.body.address,
+        opening_hours: req.body.opening_hours,
+        description: req.body.description
+      })
+      .then((restaurant) => {
+        req.flash('success_messages', 'restaurant was successfully to update')
+        res.redirect('/admin/restaurants')
+      })
+    })
   }
 
 
