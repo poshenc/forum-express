4f4e2027ea42945882703d91f523792279fbd994
johnson <poshenc@gmail.com>
2021-09-08 15:56:43 +0800

    DELETE /api/admin/restaurants/:id

--- controllers/api/adminController.js
+++ controllers/api/adminController.js
@@ -14,6 +14,14 @@ const adminController = {
     adminService.getRestaurant(req, res, (data) => {
       return res.json(data)
     })
+  },
+
+  deleteRestaurant: (req, res) => {
+    adminService.deleteRestaurant(req, res, (data) => {
+      if (data['status'] === 'success') {
+        return res.redirect('/admin/restaurants')
+      }
+    })
   }
 }
 
