3a5c6b1fa68f16981642358a8f22de67bfece612
johnson <poshenc@gmail.com>
2021-09-08 17:30:58 +0800

    DELETE /api/admin/categories/:id

--- services/categoryService.js
+++ services/categoryService.js
@@ -50,6 +50,16 @@ let categoryService = {
         })
     }
   },
+
+  deleteCategory: (req, res, callback) => {
+    return Category.findByPk(req.params.id)
+      .then((category) => {
+        category.destroy()
+          .then((category) => {
+            callback({ status: 'success', message: 'category was successfully deleted' })
+          })
+      })
+  }
 }
 
 module.exports = categoryService
